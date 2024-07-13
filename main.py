75aa0a3cf47e8a5b70718abd0579fe3de0f254d0
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-13 16:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with st.sidebar:
 # Pages based on selected option
 if selected == "Home":
     st.title("**Welcome to the Churn Prediction App!**")
-    st.image("sankarf_adhar.jpg", caption='Sankarf Adhar')
+    st.image("sankarf_adhar.jpg")
 
     
 
