88d5af60caed240886775226fc96285d8323b395
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-20 00:28:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 import streamlit as st
 import time
-from streamlit_option_menu import option_menu
-import streamlit as st
-from datetime import datetime
-
 
 df=None
+
+
+
 
 
 
@@ -19,21 +19,20 @@ df=None
 with st.sidebar:
     selected = option_menu(
         menu_title="Main Menu",  # required
-        options=["Home", "Data", "About"],  # required
-        icons=["house", "bar-chart", "info-circle"],  # optional
-        menu_icon="cast",  # optional
-        default_index=0,  # optional
+        options=["Home", "Data", "Register/Login/Profile","history"],  # required
+        icons=["house", "bar-chart", "person-square","clock-history"],  # optional
+        menu_icon="Icon font",  # optional
+        default_index=2,  # optional
     )
 
 # Pages based on selected option
 if selected == "Home":
-
     st.title("**Welcome to the Churn Prediction App!**")
+ 
+
 
     
-        
     
-        
 
 
     prediction_method = st.radio('Select Prediction Method', ('Predict Churn Record-wise', 'Predict Churn for Entire DataFrame'))
@@ -62,13 +61,13 @@ if selected == "Home":
                     'NC', 'WA', 'MN', 'NM', 'NV', 'DC', 'VT', 'KY', 'ME', 'MS', 'AL',
                     'NE', 'KS', 'TN', 'IL', 'PA', 'CT', 'ND']
             selected_states = st.selectbox("**Select states**", states)
-            st.write("Selected states:", selected_states)
+           # st.write("Selected states:", selected_states)
 
         # Area code selection
         with c1:
             area_codes = ["415", "408", "510"]
             selected_code = st.selectbox("**select an area code**", area_codes)
-            st.write("Selected area code:", selected_code)
+            #st.write("Selected area code:", selected_code)
 
         # International plan selection
         with c1:
@@ -80,9 +79,9 @@ if selected == "Home":
                 international_plan = "yes"
             elif international_no:
                 international_plan = "no"
-            else:
-                international_plan = "Not selected"
-            st.write("Selected international plan:", international_plan)
+            #else:
+               # international_plan = "Not selected"
+            #st.write("Selected international plan:", international_plan)
 
         # Voice mail plan selection
         with c1:
@@ -94,9 +93,9 @@ if selected == "Home":
                 voice_mail_plan = "yes"
             elif voice_mail_no:
                 voice_mail_plan = "no"
-            else:
-                voice_mail_plan = "Not selected"
-            st.write("Selected voice mail plan:", voice_mail_plan)
+            #else:
+             #   voice_mail_plan = "Not selected"
+            #st.write("Selected voice mail plan:", voice_mail_plan)
 
         # Input values
         variables = [
@@ -114,7 +113,7 @@ if selected == "Home":
             if i <=4:
                 with c2:
                     input_value = st.number_input(f"**{variable}**", step=None)
-                    st.write(f"{variable}: {input_value}")
+                   # st.write(f"{variable}: {input_value}")
                     dic[variable] = input_value
             elif i >4 and i <=9:
                 with c3:
@@ -124,7 +123,7 @@ if selected == "Home":
             elif i >9 and i <=14:
                 with c4:
                     input_value = st.number_input(f"**{variable}**", step=None)
-                    st.write(f"{variable}: {input_value}")
+                   # st.write(f"{variable}: {input_value}")
                     dic[variable] = input_value
 
             
@@ -132,7 +131,7 @@ if selected == "Home":
             
                 with c5:
                     input_value = st.number_input(f"**{variable}**", step=None)
-                    st.write(f"{variable}: {input_value}")
+                   # st.write(f"{variable}: {input_value}")
                     dic[variable] = input_value
 
             
@@ -140,7 +139,7 @@ if selected == "Home":
             
                 with c6:
                     input_value = st.number_input(f"**{variable}**", step=None)
-                    st.write(f"{variable}: {input_value}")
+                  #  st.write(f"{variable}: {input_value}")
                     dic[variable] = input_value
 
         # calicutating some extracted featiures
@@ -247,7 +246,15 @@ if selected == "Home":
                     st.write(f"No of churn customers: {churn_counts['Yes']}")
                     st.write(f"Total customers: {len(churn)}")
                     st.title("Go to Data tab to view analytics")
-                    df.to_csv("df.csv")
+                    df1=pd.read_csv("df.csv")
+                    df_combined = pd.concat([df,df1.head(0)], ignore_index=True)
+
+
+                    df.to_csv("df.csv",index=False)
+                    df1=pd.read_csv("df.csv")
+                    st.write(df1)
+
+
                 except Exception as e:
                         st.error(f"An error occurred during prediction: {e}")
                 
@@ -286,45 +293,127 @@ elif selected == "Data":
     st.pyplot()
 
 
+elif selected == "Register/Login/Profile":
 
+# Path to your background image
+    bg_image_path = r"login_image.jpg"
+    import base64
 
+    def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
 
-elif selected == "About":
-    st.write("hi")
-   # current = datetime.now()
-   # st.write(current)
-   # if current >= datetime(2024, 7, 15, 23, 35, 0):
-    #    st.title("Happy birthday Jeevan Reddy")
-     #   st.snow()
+    bg_image_base64 = get_base64_of_bin_file(bg_image_path)
 
-    st.title("About")
-    st.write("This is an example Streamlit app with navigation.")
-    options = ["Login", "Create New Account"]
+    st.markdown(f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpg;base64,{bg_image_base64}");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """, unsafe_allow_html=True)
 
-    # Dropdown menu
-   # sel = st.selectbox("Select an option:", options)
-    if st.button("login"):
-        
-        user_name=st.text_input("enter the user name")
-        password=st.text_input("enter tuhe user name",type="password")
-        st.button("logint")
-            #st.write(user_name)
-            #ok=st.button("logout")
-    
-
-
-    if st.button("register"):
-        user_name=st.text_input("enter the user name")
-        number=st.number_input("enter the phone number")
-        mail=st.text_input("enter the mail")
-        password=st.text_input("enter the password",type="password")
-        password=st.text_input("enter the password again",type="password")
-        st.button("register")
-
-    if st.button("view profile"):
-        st.write(user_name)
+    # Your Streamlit app content
+    #st.markdown('<style>body { margin: 0; padding: 0; overflow: hidden; }</style>', unsafe_allow_html=True)
+   # import streamlit as st
 
     
 
+    st.markdown('<h2 style="color:orange;">Welcome To Churn Prediction Application</h2>', unsafe_allow_html=True)
 
-    
+    #st.title("hi")
+
+
+procees_for_registration=True
+
+
+with st.container():
+    st.markdown('<p style="color:red;">To access the app please Login or Signup</p>', unsafe_allow_html=True)
+    st.markdown('<p style="color:red;">Select an option:</p>', unsafe_allow_html=True)
+    option = st.selectbox('', ("Signup", 'Login'))
+
+import streamlit as st
+import re
+import pandas as pd
+
+
+col1, col2 = st.columns(2)
+
+if option == "Signup":
+    with col1:
+        st.markdown('<p style="color:gold;">Enter The First Name:</p>', unsafe_allow_html=True)
+        first_name = st.text_input("", key="first_name")
+        st.markdown('<p style="color:gold;">Enter The Surname:</p>', unsafe_allow_html=True)
+        sur_name = st.text_input("", key="sur_name")
+        st.markdown('<p style="color:gold;">Enter The Mail</p>', unsafe_allow_html=True)
+        mail = st.text_input("", key="mail")
+
+        def is_valid_email(email):
+            pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+            return pattern.match(email) is not None
+
+        if is_valid_email(mail):
+            st.markdown('<p style="color:green;">The email address is valid</p>', unsafe_allow_html=True)
+            mail_val = True
+        else:
+            st.markdown('<p style="color:red;">The email address is invalid</p>', unsafe_allow_html=True)
+            mail_val = False
+
+        def is_valid_password(password):
+            pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$')
+            return pattern.match(password) is not None
+
+        st.markdown('<p style="color:gold;">Enter the password</p>', unsafe_allow_html=True)
+        password = st.text_input("", key="password", type="password")
+
+        if is_valid_password(password):
+            st.markdown('<p style="color:green;">The password is valid</p>', unsafe_allow_html=True)
+            password_val = True
+        else:
+            st.markdown('<p style="color:red;">The password should have at least one lowercase letter, one uppercase letter, one digit, one special character (@$!%*?&) and be 8-16 characters long.</p>', unsafe_allow_html=True)
+            password_val = False
+
+        st.markdown('<p style="color:gold;">Confirm the password</p>', unsafe_allow_html=True)
+        c_password = st.text_input("", key="c_password", type="password")
+
+        if c_password == password:
+            st.markdown('<p style="color:green;">Password Is Matched</p>', unsafe_allow_html=True)
+            c_password_val = True
+        else:
+            st.markdown('<p style="color:red;">Password Is Not Matches</p>', unsafe_allow_html=True)
+            c_password_val = False
+
+    with col2:
+        st.markdown('<p style="color:gold;">Enter The Last Name:</p>', unsafe_allow_html=True)
+        last_name = st.text_input("", key="last_name")
+        st.markdown('<p style="color:gold;">Enter Your Mobile Number:</p>', unsafe_allow_html=True)
+        number = st.text_input("", key="number")
+
+        if number.isnumeric() and number[0] in "9876" and len(number) == 10:
+            st.markdown('<p style="color:green;">Number is valid</p>', unsafe_allow_html=True)
+            number_val = True
+        else:
+            st.markdown('<p style="color:red;">Number is invalid</p>', unsafe_allow_html=True)
+            number_val = False
+
+if st.button("Register"):
+    if c_password_val and password_val and mail_val and number_val:
+
+        try:
+            df_user = pd.read_csv("df_user.csv")
+            st.write(df_user)
+
+        except FileNotFoundError:
+            df_user = pd.DataFrame(columns=['first_name', 'last_name', 'sur_name', 'number', 'mail', 'password'])
+
+        new_user = [first_name, last_name, sur_name, number, mail, password]
+        df_user.loc[len(df_user)] = new_user
+
+        df_user.to_csv("df_user.csv", index=False)
+        st.markdown('<p style="color:green;">Successfully Registered</p>', unsafe_allow_html=True)
+    else:
+        st.markdown('<p style="color:red;">You Have Entered Something Wrong</p>', unsafe_allow_html=True)
