3610f47b6b80c814d06838270faccfca5561a4a6
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-20 10:14:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -390,13 +390,13 @@ elif selected == "Register/Login/Profile":
             else:
                 st.markdown('<p style="color:red;">Number is invalid</p>', unsafe_allow_html=True)
                 number_val = False
-    df_user = pd.read_csv("df_user.csv")
-    if number in df_user["number"]:
-        st.markdown('<p style="color:red;">This Number is Already Registered</p>', unsafe_allow_html=True)
-    if mail in df_user["mail"]:
-        st.markdown('<p style="color:red;">This mail is Already Registered</p>', unsafe_allow_html=True)
-    if password in df_user["password"]:
-        st.markdown('<p style="color:red;">This password is Already Registered</p>', unsafe_allow_html=True)
+        df_user = pd.read_csv("df_user.csv")
+        if number in df_user["number"]:
+            st.markdown('<p style="color:red;">This Number is Already Registered</p>', unsafe_allow_html=True)
+        if mail in df_user["mail"]:
+            st.markdown('<p style="color:red;">This mail is Already Registered</p>', unsafe_allow_html=True)
+        if password in df_user["password"]:
+            st.markdown('<p style="color:red;">This password is Already Registered</p>', unsafe_allow_html=True)
 
 
         
