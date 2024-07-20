f9be16fbf7fd3d5b40c8a2602b03c24b04a03e2b
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-21 01:31:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,22 +50,7 @@ if selected == "Home":
         st.title("**Welcome to the Churn Prediction App!**")
         prediction_method = st.radio('Select Prediction Method', ('Predict Churn Record-wise', 'Predict Churn for Entire DataFrame'))
         if prediction_method=='Predict Churn Record-wise':
-
-
-
-                dic = {}
-        
-        
-        
-                # Title and header
                 c1,c2,c3,c4,c5,c6=st.columns(6)
-        
-        
-                    
-        
-                    
-        
-        
                 with c1:
                     states = ['OH', 'NJ', 'OK', 'MA', 'MO', 'LA', 'WV', 'IN', 'RI', 'IA', 'MT',
                             'NY', 'ID', 'VA', 'TX', 'FL', 'CO', 'AZ', 'SC', 'WY', 'HI', 'NH',
@@ -73,15 +58,12 @@ if selected == "Home":
                             'NC', 'WA', 'MN', 'NM', 'NV', 'DC', 'VT', 'KY', 'ME', 'MS', 'AL',
                             'NE', 'KS', 'TN', 'IL', 'PA', 'CT', 'ND']
                     selected_states = st.selectbox("**Select states**", states)
-                   # st.write("Selected states:", selected_states)
         
                 # Area code selection
                 with c1:
                     area_codes = ["415", "408", "510"]
                     selected_code = st.selectbox("**select an area code**", area_codes)
-                    #st.write("Selected area code:", selected_code)
         
-                # International plan selection
                 with c1:
                     international_yes = st.checkbox("International Plan - Yes", key="international_yes")
                     international_no = st.checkbox("International Plan - No", key="international_no")
@@ -91,11 +73,6 @@ if selected == "Home":
                         international_plan = "yes"
                     elif international_no:
                         international_plan = "no"
-                    #else:
-                       # international_plan = "Not selected"
-                    #st.write("Selected international plan:", international_plan)
-        
-                # Voice mail plan selection
                 with c1:
                     voice_mail_yes = st.checkbox("Voice Mail Plan - Yes", key="voice_mail_yes")
                     voice_mail_no = st.checkbox("Voice Mail Plan - No", key="voice_mail_no")
@@ -105,11 +82,6 @@ if selected == "Home":
                         voice_mail_plan = "yes"
                     elif voice_mail_no:
                         voice_mail_plan = "no"
-                    #else:
-                     #   voice_mail_plan = "Not selected"
-                    #st.write("Selected voice mail plan:", voice_mail_plan)
-        
-                # Input values
                 variables = [
                     'account_length', 'number_vmail_messages', 'total_day_minutes',
                     'total_day_calls', 'total_day_charge', 'total_eve_minutes',
@@ -120,22 +92,18 @@ if selected == "Home":
                     'total_charge', 'plan_day', 'plan_weeks', 'plan_years', 'charge_day'
                 ]
         
-                # Iterate over variables and create input boxes in each column
                 for i, variable in enumerate(variables):
                     if i <=4:
                         with c2:
                             input_value = st.number_input(f"**{variable}**", step=None)
-                           # st.write(f"{variable}: {input_value}")
                             dic[variable] = input_value
                     elif i >4 and i <=9:
                         with c3:
                             input_value = st.number_input(f"**{variable}**", step=None)
-                            st.write(f"{variable}: {input_value}")
                             dic[variable] = input_value
                     elif i >9 and i <=14:
                         with c4:
                             input_value = st.number_input(f"**{variable}**", step=None)
-                           # st.write(f"{variable}: {input_value}")
                             dic[variable] = input_value
         
                     
@@ -151,10 +119,8 @@ if selected == "Home":
                     
                         with c6:
                             input_value = st.number_input(f"**{variable}**", step=None)
-                          #  st.write(f"{variable}: {input_value}")
                             dic[variable] = input_value
         
-                # calicutating some extracted featiures
         
         
         
@@ -163,15 +129,7 @@ if selected == "Home":
                 l.insert(2, selected_code)
                 l.insert(3, international_plan)
                 l.insert(4, voice_mail_plan)
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                
                 data2 = [l]
         
                 columns = ['state', 'account_length', 'area_code', 'international_plan',
@@ -187,7 +145,6 @@ if selected == "Home":
         
         
         
-                # model=pickle.load(open("strnew.pkl","rb"))
         
         
                 import os
@@ -219,59 +176,57 @@ if selected == "Home":
                             st.error(f"An error occurred during prediction: {e}")
             
                     
-            
-    if prediction_method=='Predict Churn for Entire DataFrame':
-
-        import os
-        model_path = os.path.join(os.path.dirname(__file__), "strnew.pkl")
-        try:
-            with open(model_path, "rb") as f:
-                model = pickle.load(f)
-        except FileNotFoundError:
-            st.error(f"Model file {model_path} not found.")
-        except Exception as e:
-            st.error(f"An error occurred while loading the model: {e}")
-
-        file_type = st.selectbox("Select file type", ("CSV", "Excel"))
-        #uploaded_file=None
-
-        uploaded_file = st.file_uploader(f"Upload {file_type} file", type=[file_type.lower()])
-        try:
-            df=pd.read_csv(uploaded_file)
-        except Exception as e:
-                        st.error(f"An error occurred during prediction: {e}")
-        
-
-
-        if st.button("Predict"):
-            with st.spinner("Please wait while predicting...."):
-                time.sleep(3)
-            
-            
+        if prediction_method=='Predict Churn for Entire DataFrame':
+                import os
+                model_path = os.path.join(os.path.dirname(__file__), "strnew.pkl")
                 try:
-                    result = model.predict(df)
-                    churn = ["Yes" if pred == 1 else "No" for pred in result]
-                    df["churn"] = churn
-
-                    churn_counts = df['churn'].value_counts()
-
-                    st.write(f"No of churn customers: {churn_counts['Yes']}")
-                    st.write(f"Total customers: {len(churn)}")
-                    st.title("Go to Data tab to view analytics")
-                    df1=pd.read_csv("df.csv")
-                    df_combined = pd.concat([df,df1.head(0)], ignore_index=True)
-
-
-                    df.to_csv("df.csv",index=False)
-                    df1=pd.read_csv("df.csv")
-                    st.write(df1)
-
-
+                    with open(model_path, "rb") as f:
+                        model = pickle.load(f)
+                except FileNotFoundError:
+                    st.error(f"Model file {model_path} not found.")
                 except Exception as e:
-                        st.error(f"An error occurred during prediction: {e}")
+                    st.error(f"An error occurred while loading the model: {e}")
+        
+                file_type = st.selectbox("Select file type", ("CSV", "Excel"))
+                #uploaded_file=None
+        
+                uploaded_file = st.file_uploader(f"Upload {file_type} file", type=[file_type.lower()])
+                try:
+                    df=pd.read_csv(uploaded_file)
+                except Exception as e:
+                                st.error(f"An error occurred during prediction: {e}")
                 
-            
-                
+        
+        
+                if st.button("Predict"):
+                    with st.spinner("Please wait while predicting...."):
+                        time.sleep(3)
+                    
+                    
+                        try:
+                            result = model.predict(df)
+                            churn = ["Yes" if pred == 1 else "No" for pred in result]
+                            df["churn"] = churn
+        
+                            churn_counts = df['churn'].value_counts()
+        
+                            st.write(f"No of churn customers: {churn_counts['Yes']}")
+                            st.write(f"Total customers: {len(churn)}")
+                            st.title("Go to Data tab to view analytics")
+                            df1=pd.read_csv("df.csv")
+                            df_combined = pd.concat([df,df1.head(0)], ignore_index=True)
+        
+        
+                            df.to_csv("df.csv",index=False)
+                            df1=pd.read_csv("df.csv")
+                            st.write(df1)
+        
+        
+                        except Exception as e:
+                                st.error(f"An error occurred during prediction: {e}")
+                        
+                    
+                        
 
 
 elif selected == "Data":
