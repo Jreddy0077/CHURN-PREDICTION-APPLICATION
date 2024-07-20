e18393da84a53b601a67681db220122a0d0c89bf
Jreddy0077 <127132793+Jreddy0077@users.noreply.github.com>
2024-07-20 16:46:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ elif selected == "Register/Login/Profile":
                             else:
                                 st.markdown('<p style="color:gold;">Incorrect Mobile NUmber</p>', unsafe_allow_html=True)
                                 mobile=Falsetyle="color:gold;">Incorrect Mobile NUmber</p>', unsafe_allow_html=True)
-                            mobile=False
+                                mobile=False
 
                        
 
